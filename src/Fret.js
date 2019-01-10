03430c7814b9a3354ebf9701b7fa65c46a3e32a2
nmorse <morse.nate@gmail.com>
2019-01-10 17:06:55 +0000

    mixing of colors

--- src/Fret.js
+++ src/Fret.js
@@ -4,12 +4,8 @@ import Color from 'color';
 function Fret({ note_i, hilight, noteHiLite, onClick }) {
   let color = Color('rgb(255, 255, 255)');
   color = hilight ? color.darken(0.3) : color;
-  color = noteHiLite ? Color('red').darken(0.5) : color;
+  color = noteHiLite ? Color('purple').rotate(-90).lighten(0.4).mix(color, 0.7) : color;
   const notes = ['C', 'C♯', 'D', 'D♯', 'E', 'F', 'F♯', 'G', 'G♯', 'A', 'B♭', 'B'];
-  // const opacity = noteHiLite ? "1.0" : "0.8";
-  // const sColor = noteHiLite ? "120" : "250";
-  // const color = hilight ? "rgba( 150, 50, " + sColor + ", " + opacity + ")" :
-  //   "rgba( 250, 250, " + sColor + ", " + opacity + ")";
   const style = () => ({
     backgroundColor: color
   });
